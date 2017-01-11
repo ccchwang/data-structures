6fe12b0aa1e37a58e9863564d1b42e667f3e5589
Chloe Hwang <c.chloeh@gmail.com>
2017-01-10 20:32:06 -0500

    finished in-order traversing

--- 03_bst/bst.js
+++ 03_bst/bst.js
@@ -12,45 +12,47 @@ BinarySearchTree.prototype.insert = function(value){
     while(currentRoot){
 
       if (newNode.value < currentRoot.value) {
-
         if(currentRoot.left){
           currentRoot = currentRoot.left;
-
-        } else{
-
+        }
+        else {
           currentRoot.left = newNode;
           break;
         }
-      } else {
+      }
+      else {
         if(currentRoot.right){
           currentRoot = currentRoot.right;
-        } else{
+        }
+        else {
           currentRoot.right = newNode;
           break;
         }
       }
-    }
-
+    };
     this.count++;
 };
+
 BinarySearchTree.prototype.contains = function(value){
   var currentRoot = this;
+
     while(currentRoot){
 
       if(value === currentRoot.value){
         return true;
       }
-
-      if ( value < currentRoot.value) {
-
+      //if value is smaller than currentRoot
+      if (value < currentRoot.value) {
         if(currentRoot.left){
           currentRoot = currentRoot.left;
         }
         else {
           return false;
         }
-      } else {
-        if(currentRoot.right){
+      }
+      //if value is greater than currentRoot
+      else {
+        if (currentRoot.right){
           currentRoot = currentRoot.right;
         }
         else {
@@ -60,45 +62,54 @@ BinarySearchTree.prototype.contains = function(value){
     }
 
 };
+
 BinarySearchTree.prototype.depthFirstForEach = function(fn, option){
-  //if no option, run in-order
-  //if option 'in order', run in order
-  var currentRoot = this;
-  var parent;
-  var leftIsDone = false;
 
   if(!option || option === 'in-order'){
-   //left
-   //root
-   //right
-    while(currentRoot){
+  //1) go left as far as you can
+  //2) no more left, so process current node
+  //2a-2b) set new current node (right or parent), and restart loop
+    var currentRoot = this, parent = [], leftIsDone = false;
 
-      if(currentRoot.left){
-        parent = currentRoot;
+    while (currentRoot) {
+
+    //1) go left as far as it can. each time, keep adding parent to stack.
+      if (currentRoot.left && !leftIsDone){
+        parent.push(currentRoot);
         currentRoot = currentRoot.left;
       }
+
+    //2) no more left, so process node and turn on leftIsDone.
       else {
         fn(currentRoot.value);
+        leftIsDone = true;
 
+        //**After processing node, we need to set new current node. New node can be either the right or the parent**//
+
+        //2a) Right Exists: Set right as new current and set leftIsDone to false so loop can happen all over again.
         if (currentRoot.right) {
-          parent = currentRoot;
           currentRoot = currentRoot.right;
-        } else {
-          fn(currentRoot.value)
-          currentRoot = parent;
-          leftIsDone = true;
+          leftIsDone = false;
+        }
+        //2b) No Right: Set the last immediate parent (stack, LIFO) as new current.
+        else {
+          currentRoot = parent.pop();
         }
       }
-
     }
   }
 
+
+
+
   //if option pre-order, run in pre-order
   //if option is post-order, run in post-order
-  return [];
+
 };
+
 BinarySearchTree.prototype.breadthFirstForEach = function(){};
 
 BinarySearchTree.prototype.size = function(){
   return this.count;
 };
+
